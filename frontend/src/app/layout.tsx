8a2dff9a189ead33d877c1270898cccfe443ec7b
markbakos <markbakosss@gmail.com>
2025-02-25 19:30:54 +0100

    added SessionProvider to layout and changed description

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Providers} from "@/app/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "BuddyUp",
-  description: "Find a partner for any activity.",
+  description: "Find a partner for projects, gaming, and more!",
 };
 
 export default function RootLayout({
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+          <Providers>
+              {children}
+          </Providers>
       </body>
     </html>
   );
