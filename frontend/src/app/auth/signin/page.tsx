bd8835947ad30c4785eab808d10b23b1e04d3fad
markbakos <markbakosss@gmail.com>
2025-02-25 19:56:46 +0100

    updated design of signin page

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import React, { useState } from "react"
 import { signIn } from "next-auth/react"
 import { useRouter } from "next/navigation"
 
@@ -9,10 +9,12 @@ export default function SignInPage() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = async(e: React.FormEvent) => {
         e.preventDefault()
         setError("")
+        setLoading(true)
 
         const res = await signIn("credentials", {
             email,
@@ -22,16 +24,17 @@ export default function SignInPage() {
 
         if (res?.error) {
             setError(res.error)
+            setLoading(false)
         } else {
             router.push("/")
+            setLoading(false)
         }
     }
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-500">
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-800">
             <div className="w-full max-w-md p-8 bg-white rounded shadow">
                 <h1 className="text-2xl font-bold mb-6">Sign In</h1>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -59,9 +62,12 @@ export default function SignInPage() {
                             required
                         />
                     </div>
-                    <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700">
-                        Sign In
+                    {error && <p className="text-red-600 text-sm text-center my-2">{error}</p>}
+                    <button type="submit" disabled={loading}
+                            className="w-full py-2 px-4 bg-blue-600 text-white rounded cursor-pointer hover:bg-blue-700">
+                        {!loading ? "Sign in" : "Signing in"}
                     </button>
+                    <p className="text-gray-800 text-center text-xl mt-1">Don't have an account yet?<a href="http://localhost:3000/register" className="text-blue-600 font-semibold cursor-pointer"> Sign Up</a></p>
                 </form>
             </div>
         </div>
