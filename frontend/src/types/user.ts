70ec08446ebaf6e7f888822e01f652b8a6450837
markbakos <markbakosss@gmail.com>
2025-03-20 11:33:48 +0100

    updated user interface for new user profiles

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,7 +1,60 @@
+import { Ad } from "./ads"
+
 export interface User {
     id: number
     name: string
     email: string
+    jobTitle?: string
+    shortBio?: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface UserProfile {
+    id: string
+    user: User
+    aboutMe?: string
+    skills?: string[]
+    location?: string
+    profession?: string
+    experience?: Experience[]
+    education?: Education[]
+    socialLinks?: SocialLinks[]
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface Experience {
+    company: string
+    current: boolean
+    position: string
+    startDate: Date
+    endDate?: Date
+    description?: string
+}
+
+export interface Education {
+    field: string
+    degree: string
+    current: boolean
+    startDate: Date
+    endDate?: Date
+    institution: string
+}
+
+export interface SocialLinks {
+    url: string
+    platform: string
+}
+
+export interface ForeignUserProfile {
+    id: string
+    email: string
+    name: string
+    jobTitle?: string
+    shortBio?: string
+    ads?: Ad[]
+    profile: UserProfile
     createdAt: Date
     updatedAt: Date
 }
