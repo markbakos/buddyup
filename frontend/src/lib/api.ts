e059d82b6268e504307443d34ab547521bd45aef
markbakos <markbakosss@gmail.com>
2025-04-06 18:30:10 +0200

    changed base api url to production url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getSession } from 'next-auth/react';
 
-const API_BASE_URL = 'http://localhost:4000'
+const API_BASE_URL = 'https://buddyup-backend.onrender.com'
 
 const api = axios.create({
     baseURL: API_BASE_URL,
